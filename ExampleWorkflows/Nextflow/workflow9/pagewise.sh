ab871f365d033f79a2ce2dbc1b1da68978264696
mehmedGIT <mehmed.n.mustafa@gmail.com>
2022-11-03 13:58:40 +0100

    Added cached results for workflow9

---
+++ ExampleWorkflows/Nextflow/workflow9/pagewise.sh
@@ -0,0 +1,1 @@
+nextflow run pagewise.nf --cleaning "ON" -with-report pagewise.html > pagewise_nf_output.txt &
---
+++ ExampleWorkflows/Nextflow/workflow9/serial.sh
@@ -0,0 +1,1 @@
+nextflow run serial.nf --cleaning "ON" -with-report serial.html > serial_nf_output.txt &
