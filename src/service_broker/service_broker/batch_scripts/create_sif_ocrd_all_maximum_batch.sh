e234f07fcd1ff12a8436d952004bc467123cb55f
mehmedGIT <mehmed.n.mustafa@gmail.com>
2023-04-18 13:06:08 +0200

    add sif creation batch script

---
+++ src/service_broker/service_broker/batch_scripts/create_sif_ocrd_all_maximum_batch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --constraint scratch
+#SBATCH --partition medium
+#SBATCH --cpus-per-task 16
+#SBATCH --mem 32G
+#SBATCH --time 120
+#SBATCH --output /home/users/mmustaf/jobs_output/create_sif_job-%J.txt
+
+module purge
+module load singularity
+
+hostname
+slurm_resources
+
+SINGULARITY_CACHE_DIR="/scratch1/users/${USER}/singularityCache"
+CURR_DATE=$(date +"%Y_%m_%d_%H%M")
+SIF_NAME="ocrd_all_image_${CURR_DATE}.sif"
+OCRD_ALL_MAXIMUM_IMAGE="docker://ocrd/all:maximum"
+
+if [ ! -d "$SINGULARITY_CACHE_DIR" ]; then
+  mkdir -p "$SINGULARITY_CACHE_DIR"
+fi
+
+cd "$SINGULARITY_CACHE_DIR" || exit
+singularity pull "${SIF_NAME}" ${OCRD_ALL_MAXIMUM_IMAGE}
