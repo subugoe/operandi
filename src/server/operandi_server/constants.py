9f668b6d78543cca89df2f434772d73d69182d5d
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2023-06-03 13:45:51 +0200

    Pass live server url from tests when missing

--- src/server/operandi_server/constants.py
+++ src/server/operandi_server/constants.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from dotenv import load_dotenv
 from os import getenv
 from operandi_utils import OPERANDI_LOGS_DIR
+from tests.constants import OPERANDI_SERVER_URL_LIVE
 
 __all__ = [
     'SERVER_URL',
@@ -16,5 +17,5 @@ current_time = datetime.now().strftime("%Y-%m-%d_%H-%M")
 LOG_FILE_PATH: str = f"{OPERANDI_LOGS_DIR}/server_{current_time}.log"
 LOG_LEVEL: str = "INFO"
 
-# The SERVER_URL, BASE_DIR and *_ROUTERS are used by the ResourceManagers
-SERVER_URL: str = getenv("OPERANDI_SERVER_URL_LIVE")
+# TODO: Revisit this, temporal fix for the test environment
+SERVER_URL: str = getenv("OPERANDI_SERVER_URL_LIVE", OPERANDI_SERVER_URL_LIVE)
