b9bd7e5783dd99298298ffe00fd97172e2f60f03
mehmedGIT <mehmed.n.mustafa@gmail.com>
2023-10-06 17:45:13 +0200

    harvester test entry

--- tests/tests_harvester/test_dummy.py
+++ tests/tests_harvester/test_dummy.py
@@ -1,2 +1,11 @@
-def test_dummy():
-    pass
+
+def test_post_workspace_zip(harvester, path_workspace1):
+    workspace_id = harvester.post_workspace_zip(ocrd_zip_path=path_workspace1)
+    assert workspace_id
+
+
+def test_post_workspace_url(harvester):
+    mets_url = "https://content.staatsbibliothek-berlin.de/dc/PPN631277528.mets.xml"
+    file_grp = "DEFAULT"
+    workspace_id = harvester.post_workspace_url(mets_url=mets_url, file_grp=file_grp)
+    assert workspace_id
