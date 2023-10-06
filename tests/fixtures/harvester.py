753447b83d02e820fd72b686b44f874962c05668
mehmedGIT <mehmed.n.mustafa@gmail.com>
2023-10-06 17:48:29 +0200

    refactor: harvester test files

---
+++ tests/fixtures/harvester.py
@@ -0,0 +1,13 @@
+from os import environ
+from pytest import fixture
+from operandi_harvester import Harvester
+
+
+@fixture(scope="session", name="harvester")
+def fixture_operandi_harvester(auth_harvester):
+    harvester = Harvester(
+        server_address=environ.get("OPERANDI_SERVER_URL_LIVE"),
+        auth_username=auth_harvester[0],
+        auth_password=auth_harvester[1]
+    )
+    yield harvester
