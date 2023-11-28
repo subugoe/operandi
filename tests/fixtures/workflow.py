96bb36d371c21d6a72616a9660ab37fd8ef33fb8
mehmedGIT <mehmed.n.mustafa@gmail.com>
2023-11-28 14:25:10 +0100

    add odem wf fixtures

--- tests/fixtures/workflow.py
+++ tests/fixtures/workflow.py
@@ -12,6 +12,11 @@ def fixture_path_workflow2():
     yield to_asset_path("workflows", "test_default_workflow.nf")
 
 
+@fixture(scope="package", name="path_workflow3")
+def fixture_path_workflow3():
+    yield to_asset_path("workflows", "test_odem_workflow.nf")
+
+
 @fixture(scope="package", name="bytes_workflow1")
 def fixture_bytes_workflow1(path_workflow1):
     return open(path_workflow1, 'rb')
@@ -20,3 +25,8 @@ def fixture_bytes_workflow1(path_workflow1):
 @fixture(scope="package", name="bytes_workflow2")
 def fixture_bytes_workflow2(path_workflow2):
     return open(path_workflow2, 'rb')
+
+
+@fixture(scope="package", name="bytes_workflow3")
+def fixture_bytes_workflow3(path_workflow3):
+    return open(path_workflow3, 'rb')
