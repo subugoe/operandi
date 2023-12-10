d4713bf8c82c34e538b3005d884d30d92ce29778
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2023-12-10 16:19:36 +0100

    improve wf fixtures

--- tests/fixtures/workflow.py
+++ tests/fixtures/workflow.py
@@ -2,31 +2,31 @@ from pytest import fixture
 from tests.helpers_utils import to_asset_path
 
 
-@fixture(scope="package", name="path_workflow1")
-def fixture_path_workflow1():
+@fixture(scope="package", name="template_workflow")
+def fixture_template_workflow():
     yield to_asset_path("workflows", "test_template_workflow.nf")
 
 
-@fixture(scope="package", name="path_workflow2")
-def fixture_path_workflow2():
+@fixture(scope="package", name="default_workflow")
+def fixture_default_workflow():
     yield to_asset_path("workflows", "test_default_workflow.nf")
 
 
-@fixture(scope="package", name="path_workflow3")
-def fixture_path_workflow3():
+@fixture(scope="package", name="odem_workflow")
+def fixture_odem_workflow():
     yield to_asset_path("workflows", "test_odem_workflow.nf")
 
 
-@fixture(scope="package", name="bytes_workflow1")
-def fixture_bytes_workflow1(path_workflow1):
-    return open(path_workflow1, 'rb')
+@fixture(scope="package", name="bytes_template_workflow")
+def fixture_bytes_template_workflow(template_workflow):
+    return open(template_workflow, 'rb')
 
 
-@fixture(scope="package", name="bytes_workflow2")
-def fixture_bytes_workflow2(path_workflow2):
-    return open(path_workflow2, 'rb')
+@fixture(scope="package", name="bytes_default_workflow")
+def fixture_bytes_default_workflow(default_workflow):
+    return open(default_workflow, 'rb')
 
 
-@fixture(scope="package", name="bytes_workflow3")
-def fixture_bytes_workflow3(path_workflow3):
-    return open(path_workflow3, 'rb')
+@fixture(scope="package", name="bytes_odem_workflow")
+def fixture_bytes_odem_workflow(odem_workflow):
+    return open(odem_workflow, 'rb')
