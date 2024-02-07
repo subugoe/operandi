2416970fb8da0f1ad4d28b22dc5d57b9e21b8778
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-02-07 14:37:54 +0100

    trigger ci/cd

--- src/utils/operandi_utils/hpc/constants.py
+++ src/utils/operandi_utils/hpc/constants.py
@@ -6,7 +6,9 @@ __all__ = [
 ]
 
 # TODO: Pass multiple hosts. When a host timeouts, another host should be tried
-HPC_EXECUTOR_HOST = "gwdu101.hpc.gwdg.de"  # "login-mdc.hpc.gwdg.de"
+# "login-mdc.hpc.gwdg.de"
+HPC_EXECUTOR_HOST = "gwdu101.hpc.gwdg.de"
 HPC_EXECUTOR_PROXY_HOST = "login.gwdg.de"
 HPC_TRANSFER_HOST = "transfer-scc.gwdg.de"
-HPC_TRANSFER_PROXY_HOST = "login.gwdg.de"  # "transfer.gwdg.de"
+# "transfer.gwdg.de"
+HPC_TRANSFER_PROXY_HOST = "login.gwdg.de"
