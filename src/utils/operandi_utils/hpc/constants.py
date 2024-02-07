4fa7ddff739f1449f851d61ea9a6ffde4dd0eb69
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-02-07 14:18:51 +0100

    change transfer proxy host

--- src/utils/operandi_utils/hpc/constants.py
+++ src/utils/operandi_utils/hpc/constants.py
@@ -9,4 +9,4 @@ __all__ = [
 HPC_EXECUTOR_HOST = "gwdu101.hpc.gwdg.de"  # "login-mdc.hpc.gwdg.de"
 HPC_EXECUTOR_PROXY_HOST = "login.gwdg.de"
 HPC_TRANSFER_HOST = "transfer-scc.gwdg.de"
-HPC_TRANSFER_PROXY_HOST = "transfer.gwdg.de"
+HPC_TRANSFER_PROXY_HOST = "login.gwdg.de"  # "transfer.gwdg.de"
