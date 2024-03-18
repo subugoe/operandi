e34c416989e27e074e49ba31a3cb6dae4327f7a6
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-03-18 12:38:05 +0100

    change hosts order

--- src/utils/operandi_utils/hpc/constants.py
+++ src/utils/operandi_utils/hpc/constants.py
@@ -6,7 +6,7 @@ __all__ = [
 ]
 
 # "gwdu103.hpc.gwdg.de" - bad host entry, has no access to /scratch1, but to /scratch2
-HPC_EXECUTOR_HOSTS = ["gwdu102.hpc.gwdg.de", "gwdu101.hpc.gwdg.de", "login-mdc.hpc.gwdg.de"]
+HPC_EXECUTOR_HOSTS = ["login-mdc.hpc.gwdg.de", "gwdu101.hpc.gwdg.de", "gwdu102.hpc.gwdg.de"]
 HPC_EXECUTOR_PROXY_HOSTS = ["login.gwdg.de"]
 HPC_TRANSFER_HOSTS = ["transfer-mdc.hpc.gwdg.de", "transfer-scc.gwdg.de"]
-HPC_TRANSFER_PROXY_HOSTS = ["transfer.gwdg.de", "login.gwdg.de"]
+HPC_TRANSFER_PROXY_HOSTS = ["transfer.gwdg.de"]
