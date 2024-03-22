8ecb365e5ddc9ac8d9121cb1f8719d72c1cb81ad
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-03-22 13:43:58 +0100

    adapt utils tests

--- tests/tests_utils/test_hpc_xcombined.py
+++ tests/tests_utils/test_hpc_xcombined.py
@@ -11,7 +11,7 @@ ID_WORKFLOW_JOB = f"test_wf_job_{current_time}"
 ID_WORKSPACE = f"test_ws_{current_time}"
 
 
-def test_pack_and_put_slurm_workspace(hpc_data_transfer, template_workflow):
+def test_pack_and_put_slurm_workspace(hpc_data_transfer, template_workflow_with_ms):
     # Move the test asset to actual workspaces location
     local_workspace_dir = copytree(
         src=join(to_asset_path(resource_type="workspaces", name="small_ws"), "data"),
@@ -26,7 +26,7 @@ def test_pack_and_put_slurm_workspace(hpc_data_transfer, template_workflow):
     local_slurm_workspace_zip_path = hpc_data_transfer.create_slurm_workspace_zip(
         ocrd_workspace_dir=local_workspace_dir,
         workflow_job_id=ID_WORKFLOW_JOB,
-        nextflow_script_path=template_workflow,
+        nextflow_script_path=template_workflow_with_ms,
         tempdir_prefix="test_slurm_workspace-"
     )
     assert_exists_file(local_slurm_workspace_zip_path)
@@ -49,13 +49,13 @@ def test_hpc_connector_put_batch_script(hpc_data_transfer):
     )
 
 
-def test_hpc_connector_run_batch_script(hpc_command_executor, template_workflow):
+def test_hpc_connector_run_batch_script(hpc_command_executor, template_workflow_with_ms):
     batch_script_id = "submit_workflow_job.sh"
     hpc_batch_script_path = join(hpc_command_executor.batch_scripts_dir, batch_script_id)
     slurm_job_id = hpc_command_executor.trigger_slurm_job(
         batch_script_path=hpc_batch_script_path,
         workflow_job_id=ID_WORKFLOW_JOB,
-        nextflow_script_path=template_workflow,
+        nextflow_script_path=template_workflow_with_ms,
         input_file_grp="DEFAULT",
         workspace_id=ID_WORKSPACE,
         mets_basename="mets.xml",
@@ -63,7 +63,8 @@ def test_hpc_connector_run_batch_script(hpc_command_executor, template_workflow)
         cpus=2,
         ram=16,
         nf_process_forks=2,
-        ws_pages_amount=8
+        ws_pages_amount=8,
+        use_mets_server=True
     )
     finished_successfully = hpc_command_executor.poll_till_end_slurm_job_state(
         slurm_job_id=slurm_job_id,
