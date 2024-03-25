a87b4cbdaa62a9af114c5d27cb99a6a09ec0a72e
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-03-25 11:49:47 +0100

    add tunel info to tests

--- tests/fixtures/hpc.py
+++ tests/fixtures/hpc.py
@@ -2,17 +2,17 @@ from pytest import fixture
 from operandi_utils.hpc import HPCExecutor, HPCTransfer
 
 
-@fixture(scope="package", name="hpc_data_transfer")
+@fixture(scope="module", name="hpc_data_transfer")
 def fixture_hpc_transfer_connector():
-    hpc_transfer_connector = HPCTransfer()
+    hpc_transfer_connector = HPCTransfer(tunel_host="localhost", tunel_port=4023)
     # print(hpc_transfer_connector.proxy_hosts)
     # print(hpc_transfer_connector.hpc_hosts)
     yield hpc_transfer_connector
 
 
-@fixture(scope="package", name="hpc_command_executor")
+@fixture(scope="module", name="hpc_command_executor")
 def fixture_hpc_execution_connector():
-    hpc_paramiko_connector = HPCExecutor()
+    hpc_paramiko_connector = HPCExecutor(tunel_host="localhost", tunel_port=4022)
     # print(hpc_paramiko_connector.proxy_hosts)
     # print(hpc_paramiko_connector.hpc_hosts)
     yield hpc_paramiko_connector
