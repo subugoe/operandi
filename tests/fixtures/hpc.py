2949dd841ded7072414015c51e08c65c63e4dbd8
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-03-25 11:50:57 +0100

    change test hpc ports

--- tests/fixtures/hpc.py
+++ tests/fixtures/hpc.py
@@ -4,7 +4,7 @@ from operandi_utils.hpc import HPCExecutor, HPCTransfer
 
 @fixture(scope="module", name="hpc_data_transfer")
 def fixture_hpc_transfer_connector():
-    hpc_transfer_connector = HPCTransfer(tunel_host="localhost", tunel_port=4023)
+    hpc_transfer_connector = HPCTransfer(tunel_host="localhost", tunel_port=14023)
     # print(hpc_transfer_connector.proxy_hosts)
     # print(hpc_transfer_connector.hpc_hosts)
     yield hpc_transfer_connector
@@ -12,7 +12,7 @@ def fixture_hpc_transfer_connector():
 
 @fixture(scope="module", name="hpc_command_executor")
 def fixture_hpc_execution_connector():
-    hpc_paramiko_connector = HPCExecutor(tunel_host="localhost", tunel_port=4022)
+    hpc_paramiko_connector = HPCExecutor(tunel_host="localhost", tunel_port=14022)
     # print(hpc_paramiko_connector.proxy_hosts)
     # print(hpc_paramiko_connector.hpc_hosts)
     yield hpc_paramiko_connector
