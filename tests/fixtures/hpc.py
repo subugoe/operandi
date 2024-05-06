6f2f85cf536a9beb7b53c63f724f97bdeea4e2c7
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-05-06 16:13:31 +0200

    fix test

--- tests/fixtures/hpc.py
+++ tests/fixtures/hpc.py
@@ -4,7 +4,7 @@ from operandi_utils.hpc import HPCExecutor, HPCTransfer
 
 @fixture(scope="package", name="hpc_data_transfer")
 def fixture_hpc_transfer_connector():
-    hpc_transfer_connector = HPCTransfer(tunel_host="localhost", tunel_port=14023)
+    hpc_transfer_connector = HPCTransfer(tunnel_host="localhost", tunnel_port=22)
     # print(hpc_transfer_connector.proxy_hosts)
     # print(hpc_transfer_connector.hpc_hosts)
     yield hpc_transfer_connector
@@ -12,7 +12,7 @@ def fixture_hpc_transfer_connector():
 
 @fixture(scope="package", name="hpc_command_executor")
 def fixture_hpc_execution_connector():
-    hpc_paramiko_connector = HPCExecutor(tunel_host="localhost", tunel_port=14022)
+    hpc_paramiko_connector = HPCExecutor(tunnel_host="localhost", tunnel_port=22)
     # print(hpc_paramiko_connector.proxy_hosts)
     # print(hpc_paramiko_connector.hpc_hosts)
     yield hpc_paramiko_connector
