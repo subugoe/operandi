bd154eaab11cc7e8bac249f93dc6877691025788
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-07-26 17:07:40 +0200

    fix: pass full cycle test

--- tests/integration_tests/test_full_cycle.py
+++ tests/integration_tests/test_full_cycle.py
@@ -40,7 +40,8 @@ def download_workflow_job_logs(auth_harvester, operandi, workflow_id: str, workf
                 filePtr.write(chunk)
 
 
-def _test_full_cycle(auth_harvester, operandi, service_broker, bytes_small_workspace):
+def test_full_cycle(auth_harvester, operandi, service_broker, bytes_small_workspace):
+    pass
     response = operandi.get("/")
     assert response.json()["message"] == "The home page of the OPERANDI Server"
 
