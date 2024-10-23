54906a509655bf36579c52ad1cccbf6338645810
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-10-23 16:34:36 +0200

    extend user response model

--- src/server/operandi_server/models/user.py
+++ src/server/operandi_server/models/user.py
@@ -4,8 +4,12 @@ from operandi_utils.database.models import DBUserAccount
 
 
 class PYUserAction(BaseModel):
+    institution_id: str = Field(..., description="Institution id of the user")
+    user_id: str = Field(..., description="Unique id of the user")
     email: str = Field(..., description="Email linked to this User")
     account_type: AccountType = Field(AccountType.UNSET, description="The type of this user")
+    approved_user: bool = Field(False, description="Whether the account was admin approved and fully functional")
+    details: str = Field(..., description="More details about the account")
     action: str = Field(..., description="Description of the user action")
 
     class Config:
@@ -14,7 +18,10 @@ class PYUserAction(BaseModel):
     @staticmethod
     def from_db_user_account(action: str, db_user_account: DBUserAccount):
         return PYUserAction(
+            institution_id=db_user_account.institution_id,
+            user_id=db_user_account.user_id,
             email=db_user_account.email,
             account_type=db_user_account.account_type,
+            details=db_user_account.details,
             action=action
         )
