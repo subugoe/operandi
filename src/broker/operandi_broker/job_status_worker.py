9c242d2928b5c8bec50270fb9a55badff546f8d2
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-10-24 05:00:03 +0200

    remove TODO

--- src/broker/operandi_broker/job_status_worker.py
+++ src/broker/operandi_broker/job_status_worker.py
@@ -135,7 +135,6 @@ class JobStatusWorker:
                 self.log.info(f"Increasing `pages_succeed` stat by {db_workspace.pages_amount}")
                 self.log.info(f"Total amount of `pages_succeed` stat: {db_stats.pages_succeed}")
             if new_job_state == StateJob.FAILED:
-                # TODO: Download the hpc log file here as well
                 self.log.info(f"Setting new workspace state `{StateWorkspace.READY}` of workspace_id: {workspace_id}")
                 db_workspace = sync_db_update_workspace(find_workspace_id=workspace_id, state=StateWorkspace.READY)
                 sync_db_update_workflow_job(find_job_id=self.current_message_job_id, job_state=StateJob.FAILED)
