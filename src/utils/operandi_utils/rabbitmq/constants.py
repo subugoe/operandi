ff8b4c9bed15dc0b3b20a9aab899a9ebc85f4ca8
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2024-12-05 15:37:49 +0100

    revert: rabbitmq heartbeat to 0

--- src/utils/operandi_utils/rabbitmq/constants.py
+++ src/utils/operandi_utils/rabbitmq/constants.py
@@ -6,7 +6,7 @@ RABBITMQ_QUEUE_JOB_STATUSES: str = "operandi_queue_job_statuses"
 RABBITMQ_QUEUE_USERS: str = "operandi_queue_users"
 
 # Defines how often the RabbitMQ broker will send requests to the clients to verify their live state
-HEARTBEAT: int = 60
+HEARTBEAT: int = 0
 # Wait seconds before next reconnect try
 RECONNECT_WAIT: int = 3
 # Reconnect tries before timeout
