ba1e5e4d5ac5b2012421e638badcf7af94c4a150
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2026-01-23 13:49:58 +0100

    fix: remove async from broker fixture

--- tests/fixtures/broker.py
+++ tests/fixtures/broker.py
@@ -5,8 +5,8 @@ from tests.helpers_asserts import assert_availability_db
 
 
 @fixture(scope="session", name="service_broker")
-async def fixture_operandi_broker():
-    await assert_availability_db(environ.get("OPERANDI_DB_URL"))
+def fixture_operandi_broker():
+    assert_availability_db(environ.get("OPERANDI_DB_URL"))
     service_broker = ServiceBroker(test_sbatch=True)
     yield service_broker
     service_broker.kill_workers()
