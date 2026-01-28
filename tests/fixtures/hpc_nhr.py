b69709af3e767b353c88b66bfea92dde94e3ecc6
Mehmed Mustafa <mehmed.n.mustafa@gmail.com>
2026-01-28 17:42:59 +0100

    add extra prints to stdout from HPC fixtures

--- tests/fixtures/hpc_nhr.py
+++ tests/fixtures/hpc_nhr.py
@@ -1,13 +1,19 @@
+from logging import StreamHandler, DEBUG
 from pytest import fixture
+from sys import stdout
 from operandi_utils.hpc import NHRExecutor, NHRTransfer
 
 @fixture(scope="package", name="hpc_nhr_data_transfer")
 def fixture_hpc_nhr_transfer_connector():
     hpc_transfer_connector = NHRTransfer()
+    hpc_transfer_connector.logger.addHandler(StreamHandler(stdout))
+    hpc_transfer_connector.logger.setLevel(DEBUG)
     yield hpc_transfer_connector
 
 
 @fixture(scope="package", name="hpc_nhr_command_executor")
 def fixture_hpc_nhr_execution_connector():
     hpc_paramiko_connector = NHRExecutor()
+    hpc_paramiko_connector.logger.addHandler(StreamHandler(stdout))
+    hpc_paramiko_connector.logger.setLevel(DEBUG)
     yield hpc_paramiko_connector
